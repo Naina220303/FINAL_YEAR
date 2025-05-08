a5f4f2ab6ceefec3f5d432608a7849e346e287a2
Naina Madan <140733262+Naina220303@users.noreply.github.com>
2025-05-08 19:30:07 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -21,7 +21,7 @@ output_path = os.path.join(DATASET_DIR, name)
 os.makedirs(output_path, exist_ok=True)
 
 print("[INFO] Starting video stream...")
-url="http://192.168.1.22:8080/video"
+url="http://192.168.73.243:8080/video"
 cam = cv2.VideoCapture(url)
 time.sleep(2.0)
 
